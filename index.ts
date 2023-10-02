c93331183b4d294871f69114265126094bd41b3c
Tomer Horowitz <tomerh@realcommerce.co.il>
2023-10-02 19:03:19 +0300

    fix: :lipstick: applied xo auto fixes

--- index.ts
+++ index.ts
@@ -18,74 +18,70 @@ function getConfig() {
 }
 
 async function getChangelog() {
-    try {
-        // Fetch all the tags.
-        const tags = await simpleGit.tags();
-        
-        // If there are less than 2 tags, we can't compare.
-        if (tags.all.length < 2) {
-            console.error('There are less than two tags. Unable to compare.');
-            return null;
-        }
+	try {
+		// Fetch all the tags.
+		const tags = await simpleGit.tags();
 
-        // Get the last two tags.
-        const lastTag = tags.all[tags.all.length - 1];
-        const previousTag = tags.all[tags.all.length - 2];
-		console.debug(`Using tags ${previousTag} --> ${lastTag}`)
-        
-        // Get the diff for the CHANGELOG.md file between the last two tags.
+		// If there are less than 2 tags, we can't compare.
+		if (tags.all.length < 2) {
+			console.error('There are less than two tags. Unable to compare.');
+			return null;
+		}
+
+		// Get the last two tags.
+		const lastTag = tags.all.at(-1);
+		const previousTag = tags.all.at(-2);
+		console.debug(`Using tags ${previousTag} --> ${lastTag}`);
+
+		// Get the diff for the CHANGELOG.md file between the last two tags.
 		const changelogDiff = await simpleGit.diff([`${previousTag}..${lastTag}`, '--', 'CHANGELOG.md']);
-        const addedLines = changelogDiff.split('\n').filter(line => line.startsWith('+') && !line.startsWith('+++'));
-        
-        // Convert the raw added lines to a clean changelog format:
-        return addedLines.map(line => line.substring(1)).join('\n');
-    } catch (error) {
-        console.error('Failed to get changes from CHANGELOG.md between the last two tags:', error);
-        return null;
-    }
+		const addedLines = changelogDiff.split('\n').filter(line => line.startsWith('+') && !line.startsWith('+++'));
+
+		// Convert the raw added lines to a clean changelog format:
+		return addedLines.map(line => line.slice(1)).join('\n');
+	} catch (error) {
+		console.error('Failed to get changes from CHANGELOG.md between the last two tags:', error);
+		return null;
+	}
 }
 
 const config = getConfig();
-console.debug('Running with config:', config)
+console.debug('Running with config:', config);
 
 const gitlab = new Gitlab({host: config.gitlabUrl, token: config.gitlabToken});
 const simpleGit = git({config: [`http.sslVerify=${config.sslVerify}`]});
 
 const project = await gitlab.Projects.show(config.projectId);
 const repoUrl = `${config.gitlabUrl}/${project.path_with_namespace}.git`.replace('https://', `https://gitlab-ci-token:${config.gitlabToken}@`);
-console.debug('Constructed repo URL:', repoUrl)
+console.debug('Constructed repo URL:', repoUrl);
 
 const remotes = await simpleGit.getRemotes(true);
-if (!remotes.some(remote => remote.name === 'gitlab')) 
-{
+if (!remotes.some(remote => remote.name === 'gitlab')) {
 	await simpleGit.addRemote('gitlab', repoUrl);
-	console.debug('Added git remote "gitlab"')
+	console.debug('Added git remote "gitlab"');
 }
 
-if (config.pushSourceBranch)
-{
+if (config.pushSourceBranch) {
 	await simpleGit.push(['-f', 'gitlab', `HEAD:${config.sourceBranch}`]);
-	console.log(`Pushed the current state as "${config.sourceBranch}" to `, repoUrl);
+	console.log(`Pushed the current state as "${config.sourceBranch}" to`, repoUrl);
 }
 
-if (!config.mergeDescription) 
-{
-	console.debug('No GITLAB_MERGE_DESCRIPTION provided, using CHANGELOG.md')
-	config.mergeDescription = await getChangelog()
-	console.debug(config.mergeDescription)
+if (!config.mergeDescription) {
+	console.debug('No GITLAB_MERGE_DESCRIPTION provided, using CHANGELOG.md');
+	config.mergeDescription = await getChangelog();
+	console.debug(config.mergeDescription);
 }
 
-if (config.createMergeRequest) 
-{
+if (config.createMergeRequest) {
 	const {web_url: webUrl} = await gitlab.MergeRequests.create(
 		config.projectId,
-		config.sourceBranch, 
-		config.targetBranch, 
-		`Release v${config.version}`, 
+		config.sourceBranch,
+		config.targetBranch,
+		`Release v${config.version}`,
 		{
 			removeSourceBranch: true,
 			description: config.mergeDescription,
-		}
+		},
 	);
-	console.log(`Merge request created at`, webUrl);
+	console.log('Merge request created at', webUrl);
 }
